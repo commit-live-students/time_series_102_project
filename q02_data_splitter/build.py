4cf278d21181d7112ec2a34e872dc79da92931d6
ga-ashlin-dev <ashlin+78@greyatom.com>
2018-03-09 13:29:03 +0000

    Done

--- q02_data_splitter/build.py
+++ q02_data_splitter/build.py
@@ -1,7 +1,15 @@
+
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import TimeSeriesSplit
 from greyatomlib.time_series_day_02_project.q01_load_data.build import q01_load_data
 
-def q02_data_splitter():
-    
+def q02_data_splitter(path):
+    shape, data = q01_load_data(path)
+    np.random.seed(9)
+    'write your solution here'
+    x = TimeSeriesSplit(2)
+    x.random_state = 200
+    splits = list(x.split(data))
+    return splits    
+
