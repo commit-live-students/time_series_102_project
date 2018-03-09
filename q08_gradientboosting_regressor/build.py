3869ea989e7eeaa1cb211395c858539d645a5e9a
ga-ashlin-dev <ashlin+78@greyatom.com>
2018-03-09 13:36:42 +0000

    Done

--- q08_gradientboosting_regressor/build.py
+++ q08_gradientboosting_regressor/build.py
@@ -1,3 +1,4 @@
+
 import pandas as pd
 import numpy as np
 import math
@@ -6,7 +7,22 @@ from sklearn.metrics import mean_squared_error
 from greyatomlib.time_series_day_02_project.q05_feature_engineering_part4.build import q05_feature_engineering_part4
 from greyatomlib.time_series_day_02_project.q02_data_splitter.build import q02_data_splitter
 
-fe =  ["WorkDay", "Peakhours", "Peakmonths"]
+fe =  ['WorkDay', 'Peakhours', 'Peakmonths']
 
-def q08_gradientboosting_regressor():
-    
+def q08_gradientboosting_regressor(path, columns= fe, random_state=9):
+    data = q05_feature_engineering_part4(path)
+    splits = q02_data_splitter(path)
+    'write your solution here'
+    rmse = []
+    for i in splits:
+        train = i[0]
+        valid = i[1]
+        x_train, y_train = data[columns].values[train], data['Demand'].values[train]
+        x_valid, y_valid = data[columns].values[valid], data['Demand'].values[valid]
+        model = GradientBoostingRegressor(n_estimators=200, min_samples_leaf=10, learning_rate=0.01, random_state=random_state)
+        model.fit(x_train, y_train)
+        pred = model.predict(x_valid)
+        measure = math.pow(mean_squared_error(y_valid, pred), 0.5)
+        rmse.append(measure)
+    return np.mean(rmse)
+
