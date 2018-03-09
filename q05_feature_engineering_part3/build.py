47f390ed5a990a28138fcde2ffadb3454e4e1f35
ga-ashlin-dev <ashlin+78@greyatom.com>
2018-03-09 13:36:04 +0000

    Done

--- q05_feature_engineering_part3/build.py
+++ q05_feature_engineering_part3/build.py
@@ -1,8 +1,22 @@
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from greyatomlib.time_series_day_02_project.q01_load_data.build import q01_load_data
-plt.switch_backend('agg')
 
-def q05_feature_engineering_part3():
+def q05_feature_engineering_part3(path):
+    shape, data = q01_load_data(path)    
+    'write your solution here'
+    plt.figure(figsize=(16, 6))
+    data['month'] = data['Datetime'].dt.strftime('%b')
+    month = []
+    month_names = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    for i in month_names:
+        one = data[data['month'] == i]['Demand'].values
+        month.append(one)
+    plt.boxplot(month, labels=month_names)
+    plt.xlabel('Hour')
+    plt.ylabel('Demand')
+    plt.title('Change in Electricity demand wrt to Hour')
     
+
