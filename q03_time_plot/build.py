28694f35e746db66f175fa1798272ad47da20773
ga-ashlin-dev <ashlin+78@greyatom.com>
2018-03-10 05:04:35 +0000

    Done

--- q03_time_plot/build.py
+++ q03_time_plot/build.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from greyatomlib.time_series_day_02_project.q01_load_data.build import q01_load_data
-
+plt.switch_backend('agg')
 def q03_time_plot(path):
     'write your solution here'
     shape, data = q01_load_data(path)
--- q05_feature_engineering_part2/build.py
+++ q05_feature_engineering_part2/build.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from greyatomlib.time_series_day_02_project.q01_load_data.build import q01_load_data
-
+plt.switch_backend('agg')
 
 def q05_feature_engineering_part2(path):
     shape, data = q01_load_data(path)
