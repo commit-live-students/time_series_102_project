b75d790b1e561d67ebce4ba4344c2b7281c41f6d
ga-ashlin-dev <ashlin+78@greyatom.com>
2018-03-10 05:01:31 +0000

    Done

--- q03_time_plot/build.py
+++ q03_time_plot/build.py
@@ -1,7 +1,15 @@
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from greyatomlib.time_series_day_02_project.q01_load_data.build import q01_load_data
-plt.switch_backend('agg')
 
-def q03_time_plot():
+def q03_time_plot(path):
+    'write your solution here'
+    shape, data = q01_load_data(path)
+    plt.figure(figsize=(16, 6))
+    plt.plot(data['Datetime'], data['Demand'])
+    plt.xlabel('Time')
+    plt.ylabel('Demand')
+    plt.title('Electricity demand in Australia for an Year')
+
