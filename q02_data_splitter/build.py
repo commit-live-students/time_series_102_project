2605dbba76e0fbcd55271eeef875d4acce484b38
bhushan45 <bhushan45@users.noreply.github.com>
2018-07-23 10:14:38 +0530

    Update build.py

--- q02_data_splitter/build.py
+++ q02_data_splitter/build.py
@@ -3,5 +3,5 @@ import numpy as np
 from sklearn.model_selection import TimeSeriesSplit
 from greyatomlib.time_series_day_02_project.q01_load_data.build import q01_load_data
 
-def q02_data_splitter():
+
     
