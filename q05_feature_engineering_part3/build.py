0db97a600a9d8cf94cd01b01878802a9e9545620
bhushan45 <bhushan45@users.noreply.github.com>
2018-07-23 10:18:10 +0530

    Update build.py

--- q05_feature_engineering_part3/build.py
+++ q05_feature_engineering_part3/build.py
@@ -4,5 +4,5 @@ import matplotlib.pyplot as plt
 from greyatomlib.time_series_day_02_project.q01_load_data.build import q01_load_data
 plt.switch_backend('agg')
 
-def q05_feature_engineering_part3():
+
     
