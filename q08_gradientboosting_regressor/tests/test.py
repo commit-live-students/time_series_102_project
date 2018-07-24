c7c0bc0b82a88253bc5e503efd1d08c06c195512
bhushan45 <bhushan45@users.noreply.github.com>
2018-07-24 11:08:11 +0530

    Update test.py

--- q08_gradientboosting_regressor/tests/test.py
+++ q08_gradientboosting_regressor/tests/test.py
@@ -19,7 +19,7 @@ class Testing(unittest.TestCase):
         # Input parameters tests
         args_student = getfullargspec(student)
         args_original = getfullargspec(original)
-        self.assertEqual(len(args_student[0]),len(args_original[0]),  "Expected argument(s) %d, Given %d" % ( len(args_original[0],len(args_student[0]))))
+        self.assertEqual(len(args_student[0]),len(args_original[0]), "Expected argument(s) %d, Given %d" % (len(args_original[0]),len(args_student[0]))
 
     def test_timeseries_default(self):
         args_student = getfullargspec(student)
@@ -28,5 +28,5 @@ class Testing(unittest.TestCase):
 
 
     def test_return(self):
-        self.ssertAlmostEqual(self.student_return, self.original_return, "The return values do not match expected values")
+        self.assertAlmostEqual(self.student_return, self.original_return, "The return values do not match expected values")
 
