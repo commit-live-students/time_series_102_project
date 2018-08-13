43d6931f22552e2dcc2eb5d33953cf9cdbc642e1
HardikaB <hardi.2607@gmail.com>
2018-08-13 12:29:50 +0000

    Done

--- q02_data_splitter/build.py
+++ q02_data_splitter/build.py
@@ -1,7 +1,19 @@
+# %load q02_data_splitter/build.py
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import TimeSeriesSplit
 from greyatomlib.time_series_day_02_project.q01_load_data.build import q01_load_data
 
+def q02_data_splitter(path):
+    seed=9
+    shape,df = q01_load_data(path)
+    tssf= TimeSeriesSplit(n_splits=3)
+    trainl=()
+    validl=()
+    for train_index,valid_index in tssf.split(df):
+        trainl=trainl+ tuple(train_index)
+        validl=validl + tuple(valid_index)
+    return [[trainl,trainl],[validl,validl]]
 
-    
+
+
