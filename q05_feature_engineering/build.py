1c3f89f8425c7a104c644d8dc5e83e7f4c8515a9
HardikaB <hardi.2607@gmail.com>
2018-08-29 09:31:27 +0000

    Done

--- q05_feature_engineering/build.py
+++ q05_feature_engineering/build.py
@@ -1,3 +1,4 @@
+# %load q05_feature_engineering/build.py
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -6,4 +7,12 @@ plt.switch_backend('agg')
 
 
 
+def q05_feature_engineering(path):
+    tss,df=q01_load_data(path)
+    p_corf=np.corrcoef(df['Temperature'], df['Demand'])
+    plt.scatter(df['Temperature'], df['Demand'])
+    plt.show()
+
+#path='data/elecdemand.csv'
+#q05_feature_engineering(path)
 
