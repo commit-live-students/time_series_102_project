7d1f5b71b62ddd200c2e8a8559a81c501cc1902f
abkedar <anupbhande@gmail.com>
2018-09-10 15:01:30 +0000

    Done

--- q01_load_data/build.py
+++ q01_load_data/build.py
@@ -1,7 +1,19 @@
+# %load q01_load_data/build.py
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 
+path = 'data/elecdemand.csv'
 
-
+def q01_load_data(data):
     
+    df = pd.read_csv(data)  # Load data and converting to DataFrame
+    
+    # Make the Datetime column as object and Drop the index.
+    df['Datetime'] = pd.to_datetime(df['Datetime'], format = '%Y-%m-%d')
+    #df.index = df['Datetime']
+    return (df.shape, df)
+    
+q01_load_data(path)
+
+
