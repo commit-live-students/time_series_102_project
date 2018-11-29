d95391f9af3bec7538466fd80abd34071508d5c5
ramyakrishnan86 <ramya_krishnan86@yahoo.co.in>
2018-11-29 17:04:25 +0000

    Done

--- q05_feature_engineering_part2/build.py
+++ q05_feature_engineering_part2/build.py
@@ -1,8 +1,42 @@
+# %load q05_feature_engineering_part2/build.py
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from greyatomlib.time_series_day_02_project.q01_load_data.build import q01_load_data 
-plt.switch_backend('agg')
+from greyatomlib.time_series_day_02_project.q01_load_data.build import q01_load_data
 
+path = 'data/elecdemand.csv'
 
+# def q05_feature_engineering_part2(path):
+#     shape, data = q01_load_data(path)
+#     'write your solution here'
+#     data['hour'] = data['Datetime'].dt.hour
+
+#     plt.figure(figsize=(16, 6))
+
+#     hours = []
+#     for i in range(24):
+#         one = data[data['hour'] == i]['Demand'].values
+#         hours.append(one)
+#     plt.boxplot(hours, labels=[str(i) for i in range(24)])
+#     plt.xlabel('Hour')
+#     plt.ylabel('Demand')
+#     plt.title('Change in Electricity demand wrt to Hour')
+#     plt.show()
     
+    
+def q05_feature_engineering_part2(path):
+    shape, data = q01_load_data(path)
+    'write your solution here'
+    data['hour'] = data['Datetime'].dt.hour
+    plt.figure(figsize=(16, 6))
+    hours = []
+    for i in range(24):
+        one = data[data['hour'] == i]['Demand'].values
+        hours.append(one)
+    plt.boxplot(hours, labels=[str(i) for i in range(24)])
+    plt.xlabel('Hour')
+    plt.ylabel('Demand')
+    plt.title('Change in Electricity demand wrt to Hour')   
+# q05_feature_engineering_part2(path)
+
+
