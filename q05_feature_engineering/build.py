911d7917d49923285291257d065111dc4ea6be81
rajeshbrid <rajesh_brid@hotmail.com>
2018-12-13 03:35:14 +0000

    Done

--- q05_feature_engineering/build.py
+++ q05_feature_engineering/build.py
@@ -1,8 +1,25 @@
+# %load q05_feature_engineering/build.py
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from greyatomlib.time_series_day_02_project.q01_load_data.build import q01_load_data
 plt.switch_backend('agg')
+
+path='data/elecdemand.csv'
+
+def q05_feature_engineering(path):
+    shape,df = q01_load_data(path)
+    corr=np.corrcoef(df['Temperature'], df['Demand'])
+    plt.figure(figsize=(16, 6))
+    plt.scatter(df['Temperature'], df['Demand'])
+    plt.xlabel('Temperature')
+    plt.ylabel('Demand')
+    plt.title('Temperature vs Demand')
+    plt.show()
+
+# q05_feature_engineering(path)
+
+
 
 
 
