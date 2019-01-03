9541fe8ff8f100a57825f7caf1c94a4b7467ea26
rushabh-mehta <rushabh123@gmail.com>
2019-01-03 12:24:06 +0000

    Done

--- q05_feature_engineering_part4/build.py
+++ q05_feature_engineering_part4/build.py
@@ -1,9 +1,17 @@
+# %load q05_feature_engineering_part2/build.py
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 from greyatomlib.time_series_day_02_project.q01_load_data.build import q01_load_data
 plt.switch_backend('agg')
 
-def q05_feature_engineering_part4():    
-    
+def q05_feature_engineering_part4(path):
+    df_shape,df = q01_load_data(path)
+    df['hour'] = df['Datetime'].dt.hour
+    df['month'] = df['Datetime'].dt.strftime('%b')
+    df['Peakhours'] = ((df['hour']>=6) & (df['hour']<20))*1
+    df['Peakmonths'] = df['month'].apply(lambda x : 1 if x in ['Feb', 'May', 'Jun', 'Jul', 'Aug'] else 0)
+    return df
+q05_feature_engineering_part4('data/elecdemand.csv')
+
+
