748bfa238dcec7ec587174bb8c265167935137c0
rushabh-mehta <rushabh123@gmail.com>
2019-01-03 11:05:51 +0000

    Done

--- q03_time_plot/build.py
+++ q03_time_plot/build.py
@@ -1,7 +1,14 @@
+# %load q03_time_plot/build.py
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from greyatomlib.time_series_day_02_project.q01_load_data.build import q01_load_data
 plt.switch_backend('agg')
+import datetime
+
+def q03_time_plot(path):
+    df_shape,df = q01_load_data(path)
+    plt.plot(df['Datetime'],df['Demand'])
+q03_time_plot('data/elecdemand.csv')
 
 
