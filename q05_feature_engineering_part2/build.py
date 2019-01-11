9e9af61c7b6cd62748b113225e81abe3cccc2e28
rajeshbrid <rajesh_brid@hotmail.com>
2019-01-11 04:00:40 +0000

    Done

--- q05_feature_engineering_part2/build.py
+++ q05_feature_engineering_part2/build.py
@@ -6,27 +6,8 @@ from greyatomlib.time_series_day_02_project.q01_load_data.build import q01_load_
 
 path = 'data/elecdemand.csv'
 
-# def q05_feature_engineering_part2(path):
-#     shape, data = q01_load_data(path)
-#     'write your solution here'
-#     data['hour'] = data['Datetime'].dt.hour
-
-#     plt.figure(figsize=(16, 6))
-
-#     hours = []
-#     for i in range(24):
-#         one = data[data['hour'] == i]['Demand'].values
-#         hours.append(one)
-#     plt.boxplot(hours, labels=[str(i) for i in range(24)])
-#     plt.xlabel('Hour')
-#     plt.ylabel('Demand')
-#     plt.title('Change in Electricity demand wrt to Hour')
-#     plt.show()
-    
-    
 def q05_feature_engineering_part2(path):
     shape, data = q01_load_data(path)
-    'write your solution here'
     data['hour'] = data['Datetime'].dt.hour
     plt.figure(figsize=(16, 6))
     hours = []
@@ -36,7 +17,8 @@ def q05_feature_engineering_part2(path):
     plt.boxplot(hours, labels=[str(i) for i in range(24)])
     plt.xlabel('Hour')
     plt.ylabel('Demand')
-    plt.title('Change in Electricity demand wrt to Hour')   
+    plt.title('Change in Electricity demand wrt to Hour') 
+    
 # q05_feature_engineering_part2(path)
 
 
