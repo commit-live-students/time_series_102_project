54845b4162d641b9662bea575d1b4d755a0ad8cf
rajeshbrid <rajesh_brid@hotmail.com>
2019-01-11 04:04:54 +0000

    Done

--- q05_feature_engineering_part2/build.py
+++ q05_feature_engineering_part2/build.py
@@ -3,22 +3,17 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from greyatomlib.time_series_day_02_project.q01_load_data.build import q01_load_data
-
-path = 'data/elecdemand.csv'
+plt.switch_backend('agg')
 
 def q05_feature_engineering_part2(path):
-    shape, data = q01_load_data(path)
-    data['hour'] = data['Datetime'].dt.hour
-    plt.figure(figsize=(16, 6))
+    df_shape, df = q01_load_data(path)
+    df['hour'] = df['Datetime'].dt.hour
     hours = []
     for i in range(24):
-        one = data[data['hour'] == i]['Demand'].values
+        one = df[df['hour'] == i]['Demand'].values
         hours.append(one)
     plt.boxplot(hours, labels=[str(i) for i in range(24)])
-    plt.xlabel('Hour')
-    plt.ylabel('Demand')
-    plt.title('Change in Electricity demand wrt to Hour') 
+    plt.show()
     
-# q05_feature_engineering_part2(path)
 
 
