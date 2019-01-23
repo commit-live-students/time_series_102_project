31a01834747d3ac26a397de45f24779625e23ddf
sannidh <soniknitr@gmail.com>
2019-01-23 02:49:31 +0000

    Done

--- q02_data_splitter/build.py
+++ q02_data_splitter/build.py
@@ -1,7 +1,18 @@
+# %load q02_data_splitter/build.py
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import TimeSeriesSplit
 from greyatomlib.time_series_day_02_project.q01_load_data.build import q01_load_data
 
+def q02_data_splitter(path):
+    path = 'data/elecdemand.csv'
+    shape, df = q01_load_data(path)
+    tscv = TimeSeriesSplit(n_splits=2)
+    com_idx = []
+    for train_index, valid_index in tscv.split(df):
+        com_idx.append((train_index, valid_index))
+    return com_idx
 
-    
+
+
+
