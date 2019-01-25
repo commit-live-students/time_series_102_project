9f43567dc7641718774c492981ff30f6a80a2a11
sannidh <soniknitr@gmail.com>
2019-01-25 01:34:10 +0000

    Done

--- q07_randomforest_regressor/build.py
+++ q07_randomforest_regressor/build.py
@@ -1,3 +1,4 @@
+# %load q07_randomforest_regressor/build.py
 import pandas as pd
 import numpy as np
 import math
@@ -6,7 +7,23 @@ from sklearn.metrics import mean_squared_error
 from greyatomlib.time_series_day_02_project.q05_feature_engineering_part4.build import q05_feature_engineering_part4
 from greyatomlib.time_series_day_02_project.q02_data_splitter.build import q02_data_splitter
 
-fe =  ["WorkDay", "Peakhours", "Peakmonths"]
+fe =  ['WorkDay', 'Peakhours', 'Peakmonths']
+
+def q07_randomforest_regressor(path, columns = fe, random_state = 9):
+    np.random.seed(random_state)
+    data = q05_feature_engineering_part4(path)
+    com_idx =  q02_data_splitter(path)
+    rmse = []
+    for i in com_idx:
+        train_idx = i[0]
+        valid_idx = i[1]
+        X_train, y_train = data.ix[train_idx, fe], data.ix[train_idx, 'Demand']
+        X_valid, y_valid = data.ix[valid_idx, fe], data.ix[valid_idx, 'Demand']
+        model = RandomForestRegressor(n_estimators=50, min_samples_leaf=30, random_state=10)
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_valid)
+        rms = mean_squared_error(y_valid, y_pred)**0.5
+        rmse.append(rms)
+    return np.mean(rmse)
 
 
-    
